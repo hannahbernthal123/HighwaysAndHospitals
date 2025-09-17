289a94b57013c3cb2e76427864150d1456c61fa8
hannahbernthal <hannah.bernthal@menloschool.org>
2025-09-16 18:36:15 -0700

    worked on recursive function

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -19,6 +19,12 @@ public class HighwaysAndHospitals {
         // Make it n + 1 so that there isn't a city at 0.
         int[] roots = new int[n + 1];
 
+
+        // Edge case for when the hospitals are less expensive than highways, so you build a hospital in each place.
+        if (highwayCost > hospitalCost) {
+            return ((long) hospitalCost * n);
+        }
+
         // Goes through and sets the new roots.
         for (int i = 1; i <= roots.length; i++) {
             for (int j = 1; j <= cities.length; j++) {
@@ -29,17 +35,37 @@ public class HighwaysAndHospitals {
 
         }
 
+        for (int i = 0; i < cities.length; i++) {
+            int[] tempCity;
 
-        // Edge case for when the hospitals are less expensive than highways, so you build a hospital in each place.
-        if (highwayCost > hospitalCost) {
-            return ((long) hospitalCost * n);
+
+        }
+
+        // This will go through the array after the roots have been sorted and count how many individual ecosystems there are.
+        int ecosystemCount = 0;
+        // The loop starts at 1 because there is no city labeled 0.
+        for (int i = 1; i <= roots.length; i++) {
+            if (roots[i] == 0) {
+                ecosystemCount++;
+            }
         }
 
 
 
 
 
-
         return -1;
+    }
+
+    // Recursive function to find the root of the node passed in.
+    public static int find(int i, int[] roots) {
+        // Base case
+        if (roots[i] == 0) {
+            return i;
+        }
+
+        // Recursive case
+        roots[i] = find(roots[i], roots);
+        return roots[i];
     }
 }
